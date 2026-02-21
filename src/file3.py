7de2e98c96e72b41082150760b4fb1b695101664
JAYADHEEP356R <jayadheep.r@diggibyte.com>
2026-02-21 20:21:53 +0530

    inserting values in dataframe in-memory

--- src/file3.py
+++ src/file3.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructField, StringType, IntegerType, DateType, StructType
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DateType
+from datetime import date
 import os
 
 
@@ -7,30 +8,25 @@ os.environ['PYSPARK_PYTHON'] = r'C:\Users\raghu\PycharmProjects\Stages_Tasks\.ve
 os.environ['PYSPARK_DRIVER_PYTHON'] = r'C:\Users\raghu\PycharmProjects\Stages_Tasks\.venv\Scripts\python.exe'
 
 
-#create empty dataframe with schema...
-
-#there are three ways to create a empty dataframe
-
 Sparkobj = SparkSession.builder.appName("dataframe").getOrCreate()
 
-schema = StructType ([
+schema = StructType([
+    StructField("id", IntegerType(), True),
+    StructField("emp_name", StringType(), True),
+    StructField("date", DateType(), True)
+])
 
-    StructField("id",IntegerType(),True),
-    StructField("emp_name",StringType(),True),
-    StructField("date",DateType(),True),
+# empty df
+rdd = Sparkobj.sparkContext.emptyRDD()
+df1 = Sparkobj.createDataFrame(rdd, schema)
 
-]
+# correct new row (use datetime.date for DateType)
+new_row = Sparkobj.createDataFrame(
+    [(1, "jai", date(2004, 12, 20))],
+    schema
 )
 
-df = Sparkobj.createDataFrame([],schema)
+# assign union result
+df1 = df1.union(new_row)
 
-print(df.show())
-
-
-#create dataframe from the rdd
-
-rdd  = Sparkobj.sparkContext.emptyRDD()
-
-df1 = Sparkobj.createDataFrame(rdd,schema)
-
-print(df1.show())
+df1.show()
